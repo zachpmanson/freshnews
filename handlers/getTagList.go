7377c8e931ed68c3bd3824ba435c74cec0330055
Zach Manson <zachpmanson@gmail.com>
2025-10-15 00:41:50 +1100

    Proper tag support

--- handlers/getTagList.go
+++ handlers/getTagList.go
@@ -1,23 +1,46 @@
 package handlers
 
 import (
-	"io"
+	"encoding/json"
+	"freshnews/nc"
+	"freshnews/utils"
 	"net/http"
 )
 
+type FrTag struct {
+	ID   string `json:"id"`
+	Type string `json:"type,omitempty"`
+}
+
+type FrTagsResponse struct {
+	Tags []FrTag `json:"tags"`
+}
+
 func GetTagList(w http.ResponseWriter, r *http.Request) {
-	// if output=json set
-	if r.URL.Query().Get("output") == "json" {
-		// if output=json, return json
-		io.WriteString(w, `{"tags":[{"id":"/user/zach","title":"My News Feed"}]}`)
-		return
-	} else {
+	if r.URL.Query().Get("output") != "json" {
 		NotImplemented(w, r)
+		return
 	}
-	io.WriteString(w, `{
-		"tags": [
-		  { "id": "user/-/state/com.google/starred" },
-		  { "id": "user/-/label/m", "type": "folder" }
-		]
-	  }`)
+
+	folders, err := nc.GetFolders()
+	if err != nil {
+		utils.WriteError(w, err, 500)
+		return
+	}
+	response := FrTagsResponse{
+		Tags: []FrTag{{ID: "user/-/state/com.google/starred"}},
+	}
+	for _, folder := range folders {
+		response.Tags = append(response.Tags, FrTag{
+			ID: "user/-/label/" + folder.Name, Type: "folder",
+		})
+
+	}
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	utils.WriteBytes(w, jsonResponse, true)
 }
